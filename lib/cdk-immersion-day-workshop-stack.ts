9e93cc21e6d6fe3aea502ff35b4dcbafd6704078
Yono1112 <yuma.ohno.sss@gmail.com>
2025-09-02 13:21:50 +0900

    create API Gateway integrated Lambda function

--- lib/cdk-immersion-day-workshop-stack.ts
+++ lib/cdk-immersion-day-workshop-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
@@ -11,5 +12,9 @@ export class CdkImmersionDayWorkshopStack extends Stack {
       code: Code.fromAsset('lambda'), // code loaded from "lambda" directory
       handler: 'hello.handler', // file is "hello", function is "handler"
     })
+
+    const gateway = new LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
