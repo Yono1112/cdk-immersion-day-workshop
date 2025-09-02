4f59e4e57792ea6054e96a81fe887c7a5d7a7e55
Yono1112 <yuma.ohno.sss@gmail.com>
2025-09-02 14:31:59 +0900

    create HitCounter Lambda function

--- lib/cdk-immersion-day-workshop-stack.ts
+++ lib/cdk-immersion-day-workshop-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
 
 export class CdkImmersionDayWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,8 +14,12 @@ export class CdkImmersionDayWorkshopStack extends Stack {
       handler: 'hello.handler', // file is "hello", function is "handler"
     })
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     const gateway = new LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
   }
 }
